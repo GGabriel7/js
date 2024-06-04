e712c953c34a307362ef467882a8576251b012d7
Gabriel Ramos <gabrielnasciemtoramos002@gmail.com>
2024-06-04 16:25:28 -0300

    Aprendendo JS

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 45
+
+console.log (`Sua velocidade é de ${vel}.`)
+
+if (vel > 40) {
+    console.log('Vocé está acima da velocidade. MUlTADO!')
+}
+
+console.log('Sempre use sinto de segurança.')
